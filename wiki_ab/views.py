5cad412c22f4bb3a082b01cf24659edf8119553b
ABobrov <abobrov@mail.ru>
2013-07-03 18:06:52 +0600

    Iteration 4

--- wiki_ab/views.py
+++ wiki_ab/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView, DetailView, ListView
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.core.exceptions import ObjectDoesNotExist
 from models import WikiPage
 
 class AboutView(TemplateView):
@@ -46,15 +47,48 @@ class WikiPageView (DetailView):
     template_name = "wiki_page_detail.html"
     model = WikiPage
     slug_field = 'pg_url'
+    queryset = WikiPage._default_manager.filter (isdeleted=False)
 
     def dispatch(self, request, *args, **kwargs):
         self.kwargs['url'] = [v for k,v in self.request.META.items() if k == 'PATH_INFO'][0].split('/')
         if not self.kwargs['url'][-1]: del self.kwargs['url'][-1]
         if not self.kwargs['url'][0]: del self.kwargs['url'][0]
-        self.kwargs[self.slug_url_kwarg] = self.kwargs['url'][-1]
-        self.kwargs['parent'] = self.kwargs['url'][-2] if len(self.kwargs['url'])>1 else ''
+#        self.kwargs[self.slug_url_kwarg] = self.kwargs['url'][-1]
+#        self.kwargs['parent'] = self.kwargs['url'][-2] if len(self.kwargs['url'])>1 else ''
         return super (WikiPageView, self).dispatch(request, *args, **kwargs)
 
+    def get_object(self, queryset=None):
+        queryset = self.get_queryset()
+        slug_field = self.get_slug_field()
+        headers = []
+        for i in range(0,len(self.kwargs['url'])):
+            slug = self.kwargs['url'][i]
+            work_queryset = queryset.filter(**{slug_field: slug})
+            try:
+                obj = work_queryset.get()
+#                a = sasl
+            except ObjectDoesNotExist:
+                raise Http404(("No %(verbose_name)s found matching the query") %
+                          {'verbose_name': queryset.model._meta.verbose_name})
+            if i == 0:
+                if (obj.parent_pg_url):
+                    raise Http404('Incorrect url: /{0}/.'.format(slug))
+            elif not obj.parent_pg_url or self.kwargs['url'][i-1] != obj.parent_pg_url:
+                    raise Http404('Incorrect url: /{0}/.'.format('/'.join(self.kwargs['url'])))
+            headers.append(obj.header)
+#            elif i != 0:
+#                raise Http404('Incorrect url: /{0}/.'.format('/'.join(self.kwargs['url'])))
+        self.kwargs['parent_header']= headers[-2] if len(headers)>1 else "Root page"
+        work_queryset = queryset.filter(**{'parent_pg_url': obj.pg_url})
+        if work_queryset:
+            self.kwargs['child']=[]
+            for i in work_queryset:
+                self.kwargs['child'].append ({'pg_url':obj.pg_url+'/'+i.pg_url+'/','header':i.header})
+
+        return obj
+
     def get_context_data(self, **kwargs):
-        kwargs ['parent'] = self.kwargs['parent']
+        kwargs ['parent'] = '/{0}/'.format('/'.join(self.kwargs['url'][:-1])) if len(self.kwargs['url'])>1 else '/'
+        kwargs['parent_header'] = self.kwargs['parent_header']
+        kwargs['child'] = self.kwargs.get('child', None)
         return super(WikiPageView,self).get_context_data(**kwargs)
